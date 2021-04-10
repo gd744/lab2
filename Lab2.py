4af935d710fd3aee570b20e98df02198ca9519de
gd744 <gonzalezdavid7761@gmail.com>
2021-04-10 16:14:34 -0700

    first commit

--- Lab2.py
+++ Lab2.py
@@ -5,15 +5,3 @@ sheets / 5
 cout the result to the user
 end 
 """
-import math
-
-def calculate(sheet):
-    answer = sheet / 5
-    rounded = math.ceil(answer)
-    print("sheet is : " , sheet)
-    print("The answer is: ", answer)
-    print("rounded is: ", rounded)
-    return rounded
-
-output = calculate(11)
-print("The number of stamps needed is", output)
